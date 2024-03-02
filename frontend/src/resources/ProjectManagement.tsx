b231a723f9bf88ef0d9af6bd2caf64b0d1e0a6cc
11ai <124818466+11ai@users.noreply.github.com>
2024-03-02 11:25:32 +0200

    created on Sat Mar 02 2024 09:25:22 GMT+0000 (Coordinated Universal Time)

---
+++ frontend/src/resources/ProjectManagement.tsx
@@ -0,0 +1,119 @@
+import {
+  Datagrid,
+  List,
+  EditButton,
+  Edit,
+  SimpleForm,
+  Create,
+  SelectColumnsButton,
+  DatagridConfigurable,
+  TopToolbar,
+  CreateButton,
+  ExportButton,
+  FilterButton,
+  //Field controls
+  BooleanField,
+  DateField,
+  EmailField,
+  ImageField,
+  NumberField,
+  ReferenceField,
+  TextField,
+  UrlField,
+  //Input controls
+  BooleanInput,
+  DateInput,
+  EmailInput,
+  ImageInput,
+  NumberInput,
+  ReferenceInput,
+  TextInput,
+  UrlInput,
+  PasswordInput
+} from "react-admin";
+import { useRecordContext } from "react-admin";
+import { Grid } from '@mui/material';
+const ReadOnlyPasswordField = ({ record, source }) => {
+
+  // You can customize the way you display the password here, e.g., mask it with asterisks
+  const maskedPassword =  '********';
+
+  return (
+      <span>{maskedPassword}</span>
+  );
+};
+const ListActions = () => (
+    <TopToolbar>
+        <FilterButton />
+        <CreateButton />
+        <ExportButton />
+        <SelectColumnsButton />
+    </TopToolbar>
+);
+const ProjectManagementTitle = () => {
+  const record = useRecordContext();
+  return <span>ProjectManagement {record ? `"${ record.title }"` : ""}</span>;
+};
+
+export const ProjectManagementList = () => (
+      <List actions={<ListActions  />} filters={ResourceFilters} >
+        <DatagridConfigurable>
+          <TextField source="title" />
+
+<NumberField source="price" />
+<NumberField source="duration" />
+<TextField source="exercises" /><EditButton />
+
+        </DatagridConfigurable>
+      </List>
+      );
+
+export const ProjectManagementEdit = () => (
+                    <Edit title={<ProjectManagementTitle />}>
+                      <SimpleForm>
+                        <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 5 }}>
+                          <Grid item xs={4}>
+<TextInput source="title"   /></Grid>
+<Grid item xs={4}>
+<TextInput source="description"   /></Grid>
+<Grid item xs={4}>
+<NumberInput source="price"   /></Grid>
+<Grid item xs={4}>
+<NumberInput source="duration"   /></Grid>
+<Grid item xs={4}>
+<TextInput source="exercises"   /></Grid>
+                        </Grid>
+                      </SimpleForm>
+                    </Edit>
+                  );
+
+export const ProjectManagementCreate = () => (
+                                  <Create>
+                                    <SimpleForm>
+                                      <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 5 }}>
+                                        <Grid item xs={4}>
+<TextInput source="title"   /></Grid>
+<Grid item xs={4}>
+<TextInput source="description"   /></Grid>
+<Grid item xs={4}>
+<NumberInput source="price"   /></Grid>
+<Grid item xs={4}>
+<NumberInput source="duration"   /></Grid>
+<Grid item xs={4}>
+<TextInput source="exercises"   /></Grid>
+                                      </Grid>
+                                    </SimpleForm>
+                                  </Create>
+                                );
+
+const ResourceFilters = [
+      <TextInput source="q" label="Search" alwaysOn />,
+,
+,
+,
+,
+,
+
+    ];
+
+
