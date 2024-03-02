2595e62872e9988d1a90db448ab8a855cd95b3f5
11ai <124818466+11ai@users.noreply.github.com>
2024-03-02 11:25:27 +0200

    created on Sat Mar 02 2024 09:25:22 GMT+0000 (Coordinated Universal Time)

---
+++ frontend/src/dashboard.tsx
@@ -0,0 +1,18 @@
+import { Card, CardContent, CardHeader } from "@mui/material";
+    export const Dashboard = () => (
+      <Card>
+      <CardHeader title="Skilltech" />
+      <CardContent> 
+      Its an skilltech based solution app that provides students and working proffesional to buy these proffessional courses to upskill themselves. Along with excercises to make them practise these courses and show progress in terms of growth.
+      <p>Built with{' '}
+      <a
+        href="https://www.appgpt.tech"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        AppGPT
+      </a></p>
+      </CardContent>
+
+      </Card>
+    );
